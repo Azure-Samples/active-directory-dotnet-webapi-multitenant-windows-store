d6996dc171ba8c06061cbcf0ed6d093533b94b21
Jean-Marc Prieur <jmprieur@microsoft.com>
2019-06-12 15:02:34 +0200

    The Web API now verifies that the token used to call it as a user_impersonate scope

--- TodoListServiceMT/Controllers/TodoListController.cs
+++ TodoListServiceMT/Controllers/TodoListController.cs
@@ -36,18 +36,39 @@ namespace TodoListServiceMT.Controllers
         // Returns a list of all the saved Todo items associated to the current caller
         public IEnumerable<Todo> Get()
         {
+            VerifyScope();
+
             string owner = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
             var currentUserToDos = db.Todoes.Where(a => a.Owner == owner);
             return (currentUserToDos.ToList());
         }
 
+
         // POST api/todoapi
         // Saves in the db a new Todo item associated to the current caller
         public void Post(Todo todo)
         {
+            VerifyScope();
+
             todo.Owner = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
             db.Todoes.Add(todo);
             db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Verifies that the JWT used to call the API:
+        /// 1) has user claims
+        /// 2) has a scope claim of value "user_impersonation".
+        /// </summary>
+        private static void VerifyScope()
+        {
+            // The Scope claim tells you what permissions the client application has in the service.
+            // In this case we look for a scope value of user_impersonation, or full access to the service as the user.
+            var scopeClaim = ClaimsPrincipal.Current?.FindFirst("http://schemas.microsoft.com/identity/claims/scope");
+            if (scopeClaim == null || (!scopeClaim.Value.Contains("user_impersonation")))
+            {
+                throw new HttpResponseException(new HttpResponseMessage { StatusCode = HttpStatusCode.Unauthorized, ReasonPhrase = "The Scope claim does not contain 'user_impersonation' or scope claim not found" });
+            }
         }
     }
 }
